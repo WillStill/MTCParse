74c6025b18db7cfb1fea89e28198329ffe46a11c
William Stiller <wxs5242@psu.edu>
2024-06-12 16:39:03 -0400

    Psuedo code processes

--- SaxonData.py
+++ SaxonData.py
@@ -1,5 +1,6 @@
 import os
 from saxonche import *
+import numpy as np
 import h5py
 
 hf = h5py.File('output/output2.hdf5', 'w')
@@ -26,7 +27,7 @@ with PySaxonProcessor(license=False) as proc:
 
     #Name spaces must be defined properly for xpath use
     # xpathProc.declare_namespace("","urn:mtconnect.org:MTConnectStreams:2.0")
-
+    processDict = {}
     for xmlFileName in xmlFileList:
         print(xmlFileName)
 
@@ -38,13 +39,12 @@ with PySaxonProcessor(license=False) as proc:
 
 
         #XPATH Queries
-        processDict = {}
+
 
         deviceName = xpathProc.evaluate("//DeviceStream[not(@name = 'Agent')]/@name ! string()").__str__()
         hf_group = hf.create_group(deviceName)
 
-
-
+        subArr = []
 
         for i in range(int(xpathProc.evaluate("(count(//Samples/*) + count(//Events/*))").__str__())):
             i += 1
@@ -68,24 +68,38 @@ with PySaxonProcessor(license=False) as proc:
             attributeDict["ComponentName"] = str(processComponentName)
             #processCompName = str(processAttributeComponent) + str(processAttributeComponentName)
 
+            processList = [processName, processSequence, processComponent, processComponentName]
+
             for x in range(int(xpathProc.evaluate("((//Samples/* | //Events/*)[" + str(i) + "]/@*[not(name() = 'sequence' )] => count())").__str__())):
                 x += 1
                 # print("X", x)
 
                 AttributeName = xpathProc.evaluate("((//Samples/* | //Events/*)[" + str(i) + "]/@*[not(name() = 'sequence' )] ! name())[" + str(x) + "]") #(//Samples/*/@*[not(name() = 'sequence' )] ! name())[i]
-                AtrributeValue = xpathProc.evaluate("((//Samples/* | //Events/*)[" + str(i) + "]/@*[not(name() = 'sequence' )] ! string())[" + str(x) + "]") #(//Samples/*/@*[not(name() = 'sequence' )] ! string())[i]
-                attributeDict[AttributeName] = str(AtrributeValue)
+                AttributeValue = xpathProc.evaluate("((//Samples/* | //Events/*)[" + str(i) + "]/@*[not(name() = 'sequence' )] ! string())[" + str(x) + "]") #(//Samples/*/@*[not(name() = 'sequence' )] ! string())[i]
+                attributeDict[AttributeName] = str(AttributeValue)
+
+                # processList.append(AttributeValue)
+                # Get the AttributeName for the datasets HERE
+                # print(processList)
 
             processText = xpathProc.evaluate("(//Samples/* | //Events/*)[" + str(i) + "]/text()")  # (//Samples/*/text())[i]
             attributeDict["Text"] = processText
 
             processDict[processNameSeq] = attributeDict
 
+            # Append process information to the subArr list before it is made into a 2d array
+            subArr.append(processList)
+
+        # Make the subArr list 2 dimensional array
+        print(subArr)
+        arr2d = np.array([subArr])
+        # print(arr2d)
+
         #Sort
         # Radix Sort might be a good addition for the future? That or Quicksort me thinks
         # Python sort() is using Timsort
         sorted_processDict = {k: processDict[k] for k in sorted(processDict, key=lambda k: int(processDict[k]['Sequence']))}
-
+        # print(sorted_processDict)
 
         # for key in sorted_processDict:
         #     print(sorted_processDict[key]["Sequence"])
@@ -95,10 +109,32 @@ with PySaxonProcessor(license=False) as proc:
 
         #     hf = h5py.File('output/output2.hdf5', 'w')
 
+# Get the quantity of process elements to record and attribute values
+# Record the last dataset active
+# Record the last 2d array
+
+# A full data array is wanted for a dataset
+
+# Check the name of each process.
+# For each process that is found
+# If the process matches what is wanted
+
+# Create a dataset using the last 2d array of information
+# Clear the 2d array of information
+
+# Create an array using the shape of all processes.
+# Find a method to assign values to processes. If no values are found then assign a NULL value.
+# Record this array to be appended to the 2d array.
+
+# If the process does not match what is wanted create an array using the shape of all processes
+# Find a method to assign values to processes. If no values are found then assign a NULL value.
+# With the 1 dimensional array created, pull the last array and append to it.
+
         for key in sorted_processDict:
             if sorted_processDict[key]["Process"] in ["Orientation", "Position"]:
-                print(key)
-                obj = hf_group.create_group(key)
+                #print(key)
+                obj = hf_group.create_dataset(key, shape=100, dtype=None, data=None)
+
             else:
                 pass
                 # obj.create_dataset(key)
